d7243768f2194ddf1a18ece46440e25187e2fef2
AriDaron <arielle.daron@asurion.com>
2021-10-28 11:52:03 -0500

    linked minerals to facilities

--- scripts/FacilityMinerals.js
+++ scripts/FacilityMinerals.js
@@ -2,10 +2,12 @@ import { getFacilities, getMinerals } from "./database.js"
 import { setFacility, setMinerals } from "./database.js"
 import { getTransientState } from "./database.js"
 import { purchaseMineral } from "./database.js"
+import { getMineralFacilities } from "./database.js"
 
 const facilities = getFacilities()
 const minerals = getMinerals()
 const transient = getTransientState()
+const mineralFacilities = getMineralFacilities()
 
 document.addEventListener(
     "change",
@@ -19,28 +21,41 @@ document.addEventListener(
 
 export const facilityMineral = () => {
     let html = "<h3>"
-    
+
 
     let htmlHeader = "Facility Minerals"
 
     for (const facility of facilities) {
         if (transient.facilityId === facility.id) {
             htmlHeader = `Facility Minerals for ${facility.name}`
-        }    
+        }
     }
-    
+
     html += htmlHeader
 
     html += "</h3>"
-    
-    
-    
+
+
+
     html += "<ul>"
 
-    for (const mineral of minerals) {
-        html += `<li>
-                <input type="radio" name="mineralAmount" value="${mineral.id}" /> ${mineral.amount} tons of ${mineral.name}
-                </li>`
+    for (const mineral of mineralFacilities) {
+        if (mineral.facilityId === transient.facilityId) {
+
+            for (const min of minerals) {
+                if (mineral.mineralId === min.id) {
+                    html += `<li>
+                    <input type="radio" name="mineralAmount" value="${mineral.id}" /> 
+                    ${mineral.amount} tons of ${min.name}
+                    </li>`
+                }
+
+            }
+        }
+        // html += `<li>
+        //         <input type="radio" name="mineralAmount" value="${mineral.id}" /> 
+        //         ${mineral.amount} tons of ${mineral.name}
+        //         </li>`
     }
     html += "</ul>"
 
